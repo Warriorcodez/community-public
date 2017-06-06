832a0a7ff0e58d7fd651dfdfe5a257387b5ca2fe
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-06-06 16:50:48 -0700

    Implement a drawer

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,9 +1,29 @@
-import { AppBar } from 'material-ui';
+import { AppBar, Drawer, MenuItem } from 'material-ui';
 import React, { Component } from 'react';
 
 class Header extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      open: false
+    };
+
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  toggleDrawer() {
+    this.setState({
+      open: !this.state.open
+    });
+    console.log('this was clicked and it should be doing something');
+  }
+
+  handleClose(event) {
+    this.setState({
+      open: false
+    });
   }
 
   render () {
@@ -13,7 +33,29 @@ class Header extends Component {
           title="Community"
           style={styles.theme}
           titleStyle={styles.title}
+          onLeftIconButtonTouchTap={this.toggleDrawer}
         />
+          <Drawer
+            label={'HI'}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <AppBar
+              title="Community"
+              showMenuIconButton={false}
+              style={styles.theme}
+              titleStyle={styles.title}
+            />
+            <MenuItem
+              onTouchTap={this.handleClose}
+              primaryText="User profile"
+            />
+            <MenuItem
+              onTouchTap={this.handleClose}
+              primaryText="Logout"
+              // containerElement={<Link to="/logout"/>}
+            />
+          </Drawer>
       </div>
     );
   }
