e36c971ceb0911f21a65dfd6dfbb091f85ee3708
kvo180 <khoavo180@gmail.com>
2017-06-14 22:57:12 -0700

    Fix code to close Category filter menu upon selection

--- web/src/components/Tools.js
+++ web/src/components/Tools.js
@@ -29,11 +29,11 @@ class Tools extends Component {
     this.setState({ category: e.target.innerHTML });
     axios.get('/api/retrieveEventsByCategory?query=' + e.target.innerHTML)
     .then((data) => {
+      this.handleRequestClose();
       this.props.addEvents(data.data);
       const lat = data.data[0].lat;
       const lng = data.data[0].lng;
       this.props.changeCenter({lat: Number(lat), lng: Number(lng)});
-      this.handleRequestClose();
     })
     .catch((err) => {
       console.log(err);
